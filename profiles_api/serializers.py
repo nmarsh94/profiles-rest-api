4a15aed54e5a7441df55901548cb207c6e98300e
nmarsh94 <nicolasmarsh94@gmail.com>
2022-05-09 12:38:07 -0300

    Added user profile feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -12,7 +12,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserProfile
         fields = ('id', 'email', 'name', 'password')
-        extra_kwards = {
+        extra_kwargs = {
             'password': {
                 'write_only': True,
                 'style': {'input_type': 'password'}
@@ -28,3 +28,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
